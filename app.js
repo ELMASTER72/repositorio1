fe01d03662f79491ee97f04e8e729ce4ae60d0c9
ELMASTER72 <jeisonramirezjara420@gmail.com>
2022-05-03 15:42:06 -0500

    font: Añadido js

---
+++ app.js
@@ -0,0 +1,15 @@
+const main = document.getElementById('main-market');
+const selectproducts = document.getElementById("select-products");
+
+window.addEventListener('load',loadselect);
+
+function loadselect () {
+    products.map(element=> {
+    const opt = document.createElement('option');
+    opt.textContent = element.name;
+    opt.value = element.name;
+    selectproducts.appendChild(opt);
+});
+}
+
+products.map(element => {console.log(element.name)})
